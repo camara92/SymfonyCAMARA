77db88371047e40172b17e5ab4a0346793977d7f
camara92 <88945535+camara92@users.noreply.github.com>
2022-05-24 22:45:02 +0200

    mise à niveau

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    /**
+     * @Route("/", name="home")
+     */
     public function index(): Response
-    {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+    {   $daouda = "Daouda"; 
+
+        return $this->render('home/index.html.twig',
+    [
+        'admin'=>$daouda
+    ]);
     }
 }
